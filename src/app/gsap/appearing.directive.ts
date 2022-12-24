3191f115c1f13b1d87604d6cd6daa85a36b989d7
derenjavadyan <98952880+derenjavadyan@users.noreply.github.com>
2022-12-24 16:29:35 +0400

    directive completed

--- src/app/gsap/appearing.directive.ts
+++ src/app/gsap/appearing.directive.ts
@@ -5,27 +5,53 @@ import { gsap } from 'gsap';
   selector: '[appAppearing]',
 })
 export class AppearingDirective implements AfterViewInit {
-  @Input() delay = 0;
+  @Input() delay: number = 0;
+  @Input() className: string = '';
 
   constructor(private elemet: ElementRef) {}
+
+  spanAnime() {
+    gsap.fromTo(
+      this.className,
+      {
+        autoAlpha: 0,
+        y: '18px',
+      },
+      {
+        autoAlpha: 1,
+        duration: 1.5,
+        delay: this.delay,
+        y: '0px',
+        stagger: 0.1,
+      }
+    );
+  }
+
+  paraAnime() {
+    gsap.fromTo(
+      this.elemet.nativeElement,
+      {
+        autoAlpha: 0,
+        y: '40px',
+      },
+      {
+        autoAlpha: 1,
+        duration: 1.5,
+        delay: this.delay,
+        y: '0px',
+      }
+    );
+  }
 
   observer() {
     const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          gsap.fromTo(
-            this.elemet.nativeElement,
-            {
-              autoAlpha: 0,
-              y: '40px',
-            },
-            {
-              autoAlpha: 1,
-              duration: 1.5,
-              delay: this.delay,
-              y: '0px',
-            }
-          );
+          if (this.className === 'span') {
+            this.spanAnime();
+          } else {
+            this.paraAnime();
+          }
         }
       });
     });
