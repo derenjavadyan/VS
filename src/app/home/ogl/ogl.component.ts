3f87b52b2457edb9f4c3795826b0afb1fdd1eca7
derenjavadyan <98952880+derenjavadyan@users.noreply.github.com>
2023-01-13 21:17:56 +0400

    added cube

--- src/app/home/ogl/ogl.component.ts
+++ src/app/home/ogl/ogl.component.ts
@@ -1,11 +1,4 @@
-import {
-  Component,
-  ElementRef,
-  AfterViewInit,
-  ViewChild,
-  Renderer2,
-  HostListener,
-} from '@angular/core';
+import { Component, AfterViewInit, OnInit } from '@angular/core';
 import {
   Renderer,
   Camera,
@@ -20,7 +13,7 @@ import {
   templateUrl: './ogl.component.html',
   styleUrls: ['./ogl.component.scss'],
 })
-export class OglComponent implements AfterViewInit {
+export class OglComponent implements OnInit, AfterViewInit {
   public renderer = new Renderer({
     width: window.innerWidth,
     height: window.innerHeight,
@@ -31,66 +24,63 @@ export class OglComponent implements AfterViewInit {
     aspect: this.gl.canvas.width / this.gl.canvas.height,
   });
 
-  @ViewChild('canvas') canvas!: ElementRef;
+  constructor() {}
 
-  public scene = new Transform();
-  public geometry = new Box(this.gl);
-  public program = new Program(this.gl, {
-    vertex: /* glsl */ `
-        attribute vec3 position;
-
-        uniform mat4 modelViewMatrix;
-        uniform mat4 projectionMatrix;
-
-        void main() {
-          gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-        }
-    `,
-    fragment: /* glsl */ `
-        void main() {
-          gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
-        }
-    `,
-  });
-
-  public mesh = new Mesh(this.gl, {
-    geometry: this.geometry,
-    program: this.program,
-  });
-
-  constructor(public renderer2: Renderer2) {}
-
-  ngAfterViewInit(): void {
-    this.renderer2.appendChild(this.canvas.nativeElement, this.gl.canvas);
-
-    this.renderer;
-    this.gl;
-    this.camera;
-    this.camera.position.z = 5;
-
-    this.scene;
-    this.geometry;
-    this.program;
-    this.mesh;
-    this.mesh.setParent(this.scene);
-
-    this.resize();
-    this.mesh.setParent(this.scene);
-    requestAnimationFrame(this.update);
+  ngOnInit(): void {
+    this.cube();
   }
 
-  @HostListener('window:resize')
-  resize() {
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-    this.camera.perspective({
-      aspect: this.gl.canvas.width / this.gl.canvas.height,
+  ngAfterViewInit(): void {}
+
+  cube() {
+    const renderer = new Renderer();
+    const gl = renderer.gl;
+    document.body.appendChild(gl.canvas);
+
+    const camera = new Camera(gl);
+    camera.position.z = 5;
+
+    function resize() {
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      camera.perspective({
+        aspect: gl.canvas.width / gl.canvas.height,
+      });
+    }
+    window.addEventListener('resize', resize, false);
+    resize();
+
+    const scene = new Transform();
+
+    const geometry = new Box(gl);
+
+    const program = new Program(gl, {
+      vertex: /* glsl */ `
+            attribute vec3 position;
+
+            uniform mat4 modelViewMatrix;
+            uniform mat4 projectionMatrix;
+
+            void main() {
+                gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+            }
+        `,
+      fragment: /* glsl */ `
+            void main() {
+                gl_FragColor = vec4(1.0);
+            }
+        `,
     });
-  }
 
-  update() {
-    requestAnimationFrame(this.update);
-    this.mesh.rotation.y -= 0.04;
-    this.mesh.rotation.x += 0.03;
-    this.renderer.render({ scene: this.scene, camera: this.camera });
+    const mesh = new Mesh(gl, { geometry, program });
+    mesh.setParent(scene);
+
+    requestAnimationFrame(update);
+    function update() {
+      requestAnimationFrame(update);
+
+      mesh.rotation.y -= 0.04;
+      mesh.rotation.x += 0.03;
+      renderer.render({ scene, camera });
+    }
   }
 }
